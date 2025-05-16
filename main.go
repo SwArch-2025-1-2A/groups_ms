525a88b59889dc86006b1d61eae6cb5bcab3d9d5
Harrison Pinto <hpintom@unal.edu.co>
2025-05-16 01:30:34 -0500

    Add a simple test case for the sqlc generated code
    Co-authored-by: César Arthuro Lemos <cesarALS@users.noreply.github.com>
    Co-authored-by: Andrés Felipe Rojas Aguilar <arojasag@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,11 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"reflect"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+
+	"github.com/SwArch-2025-1-2A/backend/repository"
+
+	"github.com/google/uuid"
 )
 
-func main() {
-	original := "Hello,"
+func run() error {
+	ctx := context.Background()
 
-	fmt.Println(original, "world!")
+	conn, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	queries := repository.New(conn)
+
+	// list all events
+	events, err := queries.GetEvents(ctx)
+	if err != nil {
+		return err
+	}
+	log.Println(events)
+
+	var id pgtype.UUID = pgtype.UUID{}
+
+	if err := id.Scan(uuid.New().String()); err != nil {
+		log.Fatalln(err)
+	}
+
+	// create a user
+	insertedUser, err := queries.CreateUser(ctx, repository.CreateUserParams{
+		ID:   id,
+		Name: "John Doe",
+		ProfilePic: pgtype.Text{
+			String: "https://example.com/profile.jpg"},
+	})
+	if err != nil {
+		return err
+	}
+	log.Println("inserted:", insertedUser)
+
+	// get the user we just inserted
+	fetchedUser, err := queries.GetUserById(ctx, insertedUser.ID)
+	if err != nil {
+		return err
+	}
+
+	// prints true
+	log.Println(reflect.DeepEqual(insertedUser, fetchedUser))
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
