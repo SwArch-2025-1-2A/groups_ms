7806d5dd3cff8972315f479dab1c273e8041fc0c
Harrison Pinto <hpintom@unal.edu.co>
2025-05-16 00:22:00 -0500

    Create queries for `Event`, `Group` and `User`
    Some queries are not implemented yet, but this is a good start.
    
    Co-authored-by: César Arthuro Lemos <cesarALS@users.noreply.github.com>
    Co-authored-by: Andrés Felipe Rojas Aguilar <arojasag@users.noreply.github.com>

---
+++ db/queries/events.sql
@@ -0,0 +1,59 @@
+-- name: GetEvents :many
+SELECT * FROM "Event" 
+ORDER BY "starts_at"
+  AND "deleted_at" IS NULL;
+
+-- name: GetEventById :one
+SELECT * FROM "Event"
+WHERE "id" = $1
+  AND "deleted_at" IS NULL;
+
+-- name: GetEventsByCreator :many
+SELECT * FROM "Event"
+WHERE "user_creator_id" = $1
+  AND "deleted_at" IS NULL;
+
+-- name: GetEventsByGroup :many
+SELECT * FROM "Event"
+WHERE "group_creator_id" = $1
+  AND "deleted_at" IS NULL;
+
+-- name: GetEventsStartedInRange :many
+SELECT * FROM "Event"
+WHERE "starts_at" BETWEEN $1 AND $2
+AND "deleted_at" IS NULL;
+
+-- name: CreateEvent :one
+INSERT INTO "Event" (
+  "title",
+  "description",
+  "place",
+  "coordinates",
+  "starts_at",
+  "ends_at",
+  "capacity",
+  "user_creator_id",
+  "group_creator_id"
+) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+RETURNING *;
+
+-- name: ModifyEvent :one
+UPDATE "Event"
+SET 
+    "title" = COALESCE($2, "title"),
+    "description" = COALESCE($3, "description"),
+    "place" = COALESCE($4, "place"),
+    "coordinates" = COALESCE($5, "coordinates"),
+    "starts_at" = COALESCE($6, "starts_at"),
+    "ends_at" = COALESCE($7, "ends_at"),
+    "capacity" = COALESCE($8, "capacity")
+WHERE "id" = $1
+RETURNING *;
+
+-- name: SoftDeleteEvent :one
+UPDATE "Event"
+SET "deleted_at" = NOW()
+WHERE "id" = $1
+RETURNING *;
+
+
