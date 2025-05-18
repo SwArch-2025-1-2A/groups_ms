2fa1ac5cb9844e9ff36e4f7812755fed8958d363
Harrison Pinto <hpintom@unal.edu.co>
2025-05-18 10:16:35 -0500

    Use `gin` and implement HTTP handlers for GraphQL

--- server.go
+++ server.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/SwArch-2025-1-2A/backend/graph"
+	"github.com/gin-gonic/gin"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
 const defaultPort = "8080"
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.New(
+// graphqlHandler initializes and returns a GraphQL handler middleware for Gin.
+// It sets up a new GraphQL handler with the following configurations:
+//   - Configures basic schema with default resolvers
+//   - Enables OPTIONS, GET and POST transport methods
+//   - Implements query caching with LRU cache (1000 entries)
+//   - Enables GraphQL introspection
+//   - Enables Automatic Persisted Queries with LRU cache (100 entries)
+//
+// Returns a gin.HandlerFunc that serves GraphQL HTTP requests.
+func graphqlHandler() gin.HandlerFunc {
+	handler := handler.New(
 		graph.NewExecutableSchema(
 			graph.Config{
 				Resolvers: &graph.Resolver{},
@@ -30,20 +31,40 @@ func main() {
 		),
 	)
 
-	srv.AddTransport(transport.Options{})
-	srv.AddTransport(transport.GET{})
-	srv.AddTransport(transport.POST{})
+	handler.AddTransport(transport.Options{})
+	handler.AddTransport(transport.GET{})
+	handler.AddTransport(transport.POST{})
 
-	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+	handler.SetQueryCache(lru.New[*ast.QueryDocument](1000))
 
-	srv.Use(extension.Introspection{})
-	srv.Use(extension.AutomaticPersistedQuery{
+	handler.Use(extension.Introspection{})
+	handler.Use(extension.AutomaticPersistedQuery{
 		Cache: lru.New[string](100),
 	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	return func(c *gin.Context) {
+		handler.ServeHTTP(c.Writer, c.Request)
+	}
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// playgroundHandler returns a Gin middleware function that serves the GraphQL Playground interface.
+// The playground provides an interactive UI for testing GraphQL queries at the endpoint specified in Gin.
+// It sets up the playground to send GraphQL operations to the "/query" endpoint.
+//
+// Returns a gin.HandlerFunc that serves GraphQL HTTP requests.
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL Playground", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func main() {
+	// Setup gin
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run()
 }
